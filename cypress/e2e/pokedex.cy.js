e899e99bcd31f4cdac4e12209ffb375d1934daa6
Leo Niemi <niemi.leo@gmail.com>
2023-01-10 15:35:40 +0200

    Add e2e test

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -4,4 +4,10 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+  it('specific pokemon page can be visited', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('charmander').click()
+    cy.url().should('include', '/pokemon/charmander')
+    cy.get('[class*="pokemon-name"]').contains('charmander')
+  })
 })
