e8ae7c1e7a425d7221a74618dff8c1dc8314f92e
Leo Niemi <niemi.leo@gmail.com>
2023-01-11 09:19:41 +0200

    Deploy w/ an intentional error

--- app.js
+++ app.js
@@ -12,11 +12,11 @@ app.listen(PORT, () => {
 })
 
 app.get('/health', (req, res) => {
-  console.log(req)
-  console.log('BOOM')
+  throw 'error...'
+  // eslint-disable-next-line no-unreachable
   res.send('ok')
 })
 
 app.get('/version', (req, res) => {
-  res.send('1') // change this string to ensure a new version deployed
+  res.send('2') // change this string to ensure a new version deployed
 })
